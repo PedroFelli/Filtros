dfbd3cda670cad81c15dcb5ffc814d0548876f54
PedroFelli <pedrofellipeunai@gmail.com>
2018-05-24 23:04:29 -0300

    alteracao

--- src/main/java/mediamediana/main.java
+++ src/main/java/mediamediana/main.java
@@ -6,9 +6,9 @@ public class main {
 	public static void main(String[] args) {
 		
 		MyImage iobj = new MyImage();
-        iobj.readImage("D:/pastilhinhas.jpg");
+        iobj.readImage("");
         Mediana.medianFilter(iobj,  3);
-        iobj.writeImage("D:/tons12.jpg");
+        iobj.writeImage("");
 	}
 
 }
